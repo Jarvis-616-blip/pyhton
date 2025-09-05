11593d1583ed202ed249b9f75bd1c1b05e89b801
Rajnish <rajnish190605@gmail.com>
2025-09-05 13:18:51 +0530

    commit

---
+++ code.py
@@ -0,0 +1,18 @@
+friends = ["rajnish","abhi","sage","ryena",False,1]
+print(friends)
+print(friends[1])
+
+friends[0] = "viper"
+
+print(friends)
+print(friends[0:2])
+#list method
+friends.append("jett")
+print(friends)
+friends.sort
+print(friends)
+friends.reverse
+friends.pop(1)
+friends.remove(False)
+print(friends)
+
